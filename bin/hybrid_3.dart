e169b56121ed63efb72be0c7c65e16a97f72279b
Wallace <144630953+Zhao0148@users.noreply.github.com>
2024-09-28 14:38:59 -0400

    testing constructor

--- bin/hybrid_3.dart
+++ bin/hybrid_3.dart
@@ -1,15 +1,28 @@
 import 'package:hybrid_3/Classes/all_classes.dart';
 
-import 'dart:convert';
+import 'dart:convert' as convert;
 import 'dart:io';
 
-Future<List<Map<String, dynamic>>> readJsonFile(String filePath) async {
+Future<List<Map<String, String>>> readJsonFile(String filePath) async {
   var input = await File(filePath).readAsString();
-  var map = await jsonDecode(input);
-  return (map['fields']).cast<Map<String, dynamic>>();
+  var map = await convert.jsonDecode(input);
+// Seems like I needed to convert each field to a Map<String, String> type as it was being read as a Map<String, dynamic> type for some reason.
+  List<Map<String, String>> output = (map['fields'] as List).map((singleField) => 
+   Map<String, String>.from(singleField as Map<String, dynamic>)
+  ).toList();
+  return output;
 }
 
-void main(List<String> arguments) async {
+void main() async {
   var output = await readJsonFile('/Users/wallacezhao/Documents/Dart/hybrid-3/mock-data/mockdata.json');
-  print(output);
+
+  if(output.isNotEmpty) {
+    Student student = Student(output);
+    // student.people = output;
+    final sortedFields = student.toSort("first");
+    for( var field in sortedFields) {
+      print(field);
+    }
+
+  }
 }
--- lib/Classes/all_classes.dart
+++ lib/Classes/all_classes.dart
@@ -3,11 +3,38 @@ import 'dart:core';
 class Student {
   List<Map<String, String>> people = [];
 
-  Student() {
+  Student(List<Map<String, String>> people) {
+    this.people = people;
+    // this.people ;
     print('Student class constructor');
   }
 
-  void toSort(people) {
-    people.sort();
+  toSort(String? field) {
+    if (people.isEmpty) {
+      return [];
+    }
+// check for null values
+    var isField = people[0].keys;
+    if (isField.contains("$field")) {
+      print('first exists');
+      people.sort((a, b) {
+        if (a["$field"] != null && b["$field"] != null && a["$field"] is String && b["$field"] is String) {
+          var firstElement = a["$field"]!;
+          var secondElement = b["$field"]!;
+          var comparison =  firstElement.compareTo(secondElement);
+          print("comparison: $comparison");
+          return comparison;
+
+        } else {
+          
+          return 0;
+        }
+      });
+      
+      return people;
+    } else {
+      print('first does not exist');
+    }
+
   }
 }
