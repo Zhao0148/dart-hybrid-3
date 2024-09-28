4f9fa4b741aa32aff593e64f2c76ccc7db7d61f4
Wallace <144630953+Zhao0148@users.noreply.github.com>
2024-09-28 15:33:04 -0400

    ok

--- bin/hybrid_3.dart
+++ bin/hybrid_3.dart
@@ -7,22 +7,22 @@ Future<List<Map<String, String>>> readJsonFile(String filePath) async {
   var input = await File(filePath).readAsString();
   var map = await convert.jsonDecode(input);
 // Seems like I needed to convert each field to a Map<String, String> type as it was being read as a Map<String, dynamic> type for some reason.
-  List<Map<String, String>> output = (map['fields'] as List).map((singleField) => 
-   Map<String, String>.from(singleField as Map<String, dynamic>)
-  ).toList();
+  List<Map<String, String>> output =
+      (map['fields'] as List).map((singleField) => Map<String, String>.from(singleField as Map<String, dynamic>)).toList();
   return output;
 }
 
 void main() async {
   var output = await readJsonFile('/Users/wallacezhao/Documents/Dart/hybrid-3/mock-data/mockdata.json');
 
-  if(output.isNotEmpty) {
+  if (output.isNotEmpty) {
     Student student = Student(output);
-    // student.people = output;
-    final sortedFields = student.toSort("first");
-    for( var field in sortedFields) {
-      print(field);
-    }
-
+    student.toSort("first");
+    student.output();
+    student.plus({});
+    student.output();
+    // student.remove("Wallace");
+    print("----");
+    student.output();
   }
 }
--- lib/Classes/all_classes.dart
+++ lib/Classes/all_classes.dart
@@ -1,12 +1,12 @@
 import 'dart:core';
 
+enum ValidFields { first, last, email }
+
 class Student {
   List<Map<String, String>> people = [];
 
-  Student(List<Map<String, String>> people) {
-    this.people = people;
-    // this.people ;
-    print('Student class constructor');
+  Student(List<Map<String, String>> listOfFields) {
+    this.people = listOfFields;
   }
 
   toSort(String? field) {
@@ -15,26 +15,45 @@ class Student {
     }
 // check for null values
     var isField = people[0].keys;
+    print(isField);
     if (isField.contains("$field")) {
-      print('first exists');
       people.sort((a, b) {
         if (a["$field"] != null && b["$field"] != null && a["$field"] is String && b["$field"] is String) {
           var firstElement = a["$field"]!;
           var secondElement = b["$field"]!;
-          var comparison =  firstElement.compareTo(secondElement);
-          print("comparison: $comparison");
+          var comparison = firstElement.compareTo(secondElement);
           return comparison;
-
         } else {
-          
           return 0;
         }
       });
-      
+
       return people;
     } else {
       print('first does not exist');
     }
+  }
 
+  output() {
+    for (var field in people) {
+      print(field);
+    }
+  }
+
+  plus(Map<String, String> person) {
+    print("person: $person");
+    if (person.isEmpty) return;
+
+    bool isValid = ValidFields.values.every((field) => person.containsKey(field.name)) && person.length == ValidFields.values.length;
+    print(isValid);
+    if (isValid) {
+      people.add(person);
+    } else {
+      print('Invalid field');
+    }
+  }
+
+  remove(String field) {
+    people.removeWhere((personObject) => personObject['first'] == field);
   }
 }
